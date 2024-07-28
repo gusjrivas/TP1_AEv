8aa3c22f94c6d4b33b383cce4536bf9ede07e679
gusjrivas <gusjrivas@gmail.com>
2024-07-28 18:53:09 -0300

    Update ej1 ej2
    Se actualiza código de ejercicio 1 y 2

--- ae_tp1_e1.py
+++ ae_tp1_e1.py
@@ -1,23 +1,49 @@
 #######################################################################################
+# CEIA - 16Co2024 - Algoritmos Evolutivos - TP1 - Ejercicio 1
+# Gustavo J. Rivas (a1620) | Myrna L. Degano (a1618)
+#######################################################################################
 # Algoritmo para mutar un alelo aleatorio a aquellos genes pertenecientes a los 
 # cromosomas de A que tengan en su i-ésima fila un correspondiente de B inferior a 0.09 
 #  Guarda los resultados en un vector C y los muestra por pantalla
 #######################################################################################
 
 import random
+from tabulate import tabulate
 
+##################################################################
 # Parámetros
-TAMANIO_POBLACION = 20
-LONGITUD_CROMOSOMA = 5
+##################################################################
+
+print("\nINGRESE LOS PARÁMETROS PARA LA EJECUCIÓN DEL ALGORITMO (O <ENTER> PARA TOMAR LOS VALORES POR DEFAULT)\n")
+
+TAMANIO_POBLACION = input("TAMAÑO DE LA POBLACIÓN (DEFAULT: 20): ").strip()
+TAMANIO_POBLACION = int(TAMANIO_POBLACION) if TAMANIO_POBLACION else 20
+
+LONGITUD_CROMOSOMA  = input("LONGITUD DEL CROMOSOMA (DEFAULT: 5): ").strip()
+LONGITUD_CROMOSOMA  = int(LONGITUD_CROMOSOMA ) if LONGITUD_CROMOSOMA  else 5
+
+TASA_LIMITE  = input("TASA LÍMITE PARA LA MUTACIÓN (DEFAULT: 0.09): ").strip()
+TASA_LIMITE  = float(TASA_LIMITE) if TASA_LIMITE else 0.09
 
 ##################################################################
 # Inicializar la población
-###################################################################
+##################################################################
 def inicializar_poblacion(tamanio_poblacion, longitud_cromosoma):
+    """
+    Genera un vector aleatorio de la longitud especificada, con valores entre 0 y 1.
+    con valores binarios del tamaño especificado
+
+    Parámetros:
+    tamanio_poblacion (int): La longitud del vector.
+    longitud_cromosoma (int): La longitud del individuo
+
+    Retorna:
+    list: Un vector de individuos (cromosomas) aleatorios)
+    """
     poblacion = []
-    for z in range(tamanio_poblacion):
+    for _ in range(tamanio_poblacion):
         cromosoma = ""
-        for t in range(longitud_cromosoma):
+        for _ in range(longitud_cromosoma):
             cromosoma = cromosoma+str(random.randint(0, 1))
         poblacion.append(cromosoma)
     return poblacion
@@ -26,12 +52,12 @@ def inicializar_poblacion(tamanio_poblacion, longitud_cromosoma):
 # Generar vector B de números aleatorios entre 0 y 1
 ###################################################################
 
-def generar_vector_aleatorio(longitud=20):
+def generar_vector_aleatorio(longitud):
     """
     Genera un vector aleatorio de la longitud especificada, con valores entre 0 y 1.
     
     Parámetros:
-    longitud (int): La longitud del vector. Por defecto es 20.
+    longitud (int): La longitud del vector.
     
     Retorna:
     list: Un vector de valores aleatorios entre 0 y 1.
@@ -59,29 +85,31 @@ def mutar_cromosoma(cromosoma):
         cromosoma_mutado = cromosoma[:indice_mutacion] + '1' + cromosoma[indice_mutacion + 1:]
     else:
         cromosoma_mutado = cromosoma[:indice_mutacion] + '0' + cromosoma[indice_mutacion + 1:]
-    return cromosoma_mutado
+    return (cromosoma_mutado, indice_mutacion)
 
 ##################################################################
-# Generar población vector A de 20 individuos binarios aleatorios
+# Desarrollo del algoritmo
 ###################################################################
 
 individuos = inicializar_poblacion(TAMANIO_POBLACION, LONGITUD_CROMOSOMA)
-print("Población inicial:", individuos)
 
 vector_mutacion = generar_vector_aleatorio(TAMANIO_POBLACION)
-print("Vector de mutación:", vector_mutacion)
-
-##################################################################
-# Generar el vector C mutado según el criterio dado
-###################################################################
 
 vector_mutado = []
 
+resultados = []
+
 for i in range(TAMANIO_POBLACION):
-    if vector_mutacion[i] < 0.09:
-        vector_mutado.append(mutar_cromosoma(individuos[i]))
+    if vector_mutacion[i] < TASA_LIMITE:
+        (cromosoma_mutado, gen_mutado) = mutar_cromosoma(individuos[i])
+        gen_mutado = "Mutación del gen " + str(gen_mutado+1)
+        vector_mutado.append(cromosoma_mutado)
     else:
+        (cromosoma_mutado, gen_mutado) = (individuos[i], "Sin mutación")
         vector_mutado.append(individuos[i])
 
-print("Vector mutado:", vector_mutado)
+    resultados.append([individuos[i], vector_mutacion[i], vector_mutado[i], gen_mutado])
 
+
+headers = ["Población inicial", "Vector de mutación", "Vector mutado", "Comentarios (Pm="+ str(TASA_LIMITE)+")"]
+print(tabulate(resultados, headers=headers, tablefmt="grid"))
--- ae_tp1_e2.py
+++ ae_tp1_e2.py
@@ -1,17 +1,35 @@
-###################################################################
+####################################################################
+# CEIA - 16Co2024 - Algoritmos Evolutivos - TP1 - Ejercicio 2
+# Gustavo J. Rivas (a1620) | Myrna L. Degano (a1618)
+####################################################################
+
 # Algoritmo Genético que encuentra el máximo de la función x^2
 # Selección por ruleta
-# Pc = 0.92
-# Pm = 0.1
 ###################################################################
 import random
+from tabulate import tabulate
 
+##################################################################
 # Parámetros
-TAMANIO_POBLACION = 4
-LONGITUD_CROMOSOMA = 5
-TASA_MUTACION = 0.1
-TASA_CRUCE = 0.92
-GENERACIONES = 10
+##################################################################
+
+print("\nINGRESE LOS PARÁMETROS PARA LA EJECUCIÓN DEL ALGORITMO (O <ENTER> PARA TOMAR LOS VALORES POR DEFAULT)\n")
+
+TAMANIO_POBLACION = input("TAMAÑO DE LA POBLACIÓN (DEFAULT: 4): ").strip()
+TAMANIO_POBLACION = int(TAMANIO_POBLACION) if TAMANIO_POBLACION else 4
+
+LONGITUD_CROMOSOMA = input("LONGITUD DEL CROMOSOMA (DEFAULT: 5): ").strip()
+LONGITUD_CROMOSOMA = int(LONGITUD_CROMOSOMA) if LONGITUD_CROMOSOMA else 5
+
+TASA_CRUCE = input("PROBABILIDAD DE CRUCE (DEFAULT: 0.92): ").strip()
+TASA_CRUCE = float(TASA_CRUCE) if TASA_CRUCE else 0.92
+
+TASA_MUTACION = input("PROBABILIDAD DE MUTACIÓN (DEFAULT: 0.01): ").strip()
+TASA_MUTACION = float(TASA_MUTACION) if TASA_MUTACION else 0.01
+
+GENERACIONES = input("CANTIDAD DE GENERACIONES (DEFAULT: 10): ").strip()
+GENERACIONES = int(GENERACIONES) if GENERACIONES else 10
+
 
 ###################################################################
 # Aptitud (y = x^2)
@@ -25,15 +43,15 @@ def aptitud(cromosoma):
 ###################################################################
 def inicializar_poblacion(tamanio_poblacion, longitud_cromosoma):
     poblacion = []
-    for z in range(tamanio_poblacion):
+    for _ in range(tamanio_poblacion):
         cromosoma = ""
-        for t in range(longitud_cromosoma):
+        for _ in range(longitud_cromosoma):
             cromosoma = cromosoma+str(random.randint(0, 1))
         poblacion.append(cromosoma)
     return poblacion
 
 ###################################################################
-# Seleccion por ruleta
+# Selección por ruleta
 ###################################################################
 def seleccion_ruleta(poblacion, aptitud_total):
     seleccion = random.uniform(0, aptitud_total)
@@ -44,7 +62,7 @@ def seleccion_ruleta(poblacion, aptitud_total):
             return individuo
 
 ###################################################################
-# Cruce monopunto con probabilidad de cruza pc = 0.92
+# Cruce monopunto
 ###################################################################
 def cruce_mono_punto(progenitor1, progenitor2, tasa_cruce):
     if random.random() < tasa_cruce:
@@ -56,7 +74,7 @@ def cruce_mono_punto(progenitor1, progenitor2, tasa_cruce):
     return descendiente1, descendiente2
 
 ###################################################################
-# mutacion
+# Mutación
 ###################################################################
 def mutacion(cromosoma, tasa_mutacion):
     cromosoma_mutado = ""
@@ -68,62 +86,58 @@ def mutacion(cromosoma, tasa_mutacion):
     return cromosoma_mutado
 
 ###################################################################
-# aplicacion de operadores geneticos
+# Aplicación de operadores genéticos
 ###################################################################
 def algoritmo_genetico(tamaño_poblacion, longitud_cromosoma, tasa_mutacion, tasa_cruce, generaciones):
     poblacion = inicializar_poblacion(tamaño_poblacion, longitud_cromosoma)
 
     for generacion in range(generaciones):
-        print("Generación:", generacion + 1)
 
-        # Calcular aptitud total para luego
+        # Calcular aptitud total
         aptitud_total = 0
         for cromosoma in poblacion:
             aptitud_total = aptitud_total+aptitud(cromosoma)
 
-        print("Aptitud total:", aptitud_total)
-
-        # ..................................................................
-        # seleccion
-        # de progenitores con el metodo ruleta
-        # se crea una lista vacia de progenitores primero
+        # Selección
+        # de progenitores con el método ruleta
         progenitores = []
         for _ in range(tamaño_poblacion):
             progenitores.append(seleccion_ruleta(poblacion, aptitud_total))
 
-        # ..................................................................
         # Cruce
         descendientes = []
         for i in range(0, tamaño_poblacion, 2):
             descendiente1, descendiente2 = cruce_mono_punto(progenitores[i], progenitores[i + 1], tasa_cruce)
             descendientes.extend([descendiente1, descendiente2])
 
-        # ..................................................................
-        # mutacion
+        # Mutación
         descendientes_mutados = []
         for descendiente in descendientes:
             descendientes_mutados.append(mutacion(descendiente, tasa_mutacion))
 
-        # Aqui se aplica elitismo
-        # se reemplazar los peores cromosomas con los mejores progenitores
+        # Elitismo - se reemplazan los peores cromosomas con los mejores progenitores
         poblacion.sort(key=aptitud)
         descendientes_mutados.sort(key=aptitud, reverse=True)
         for i in range(len(descendientes_mutados)):
             if aptitud(descendientes_mutados[i]) > aptitud(poblacion[i]):
                 poblacion[i] = descendientes_mutados[i]
 
-        # mostrar el mejor individuo de la generacion
+        # Mostrar el mejor individuo de la generación
         mejor_individuo = max(poblacion, key=aptitud)
-        print("Mejor individuo:", int(mejor_individuo, 2), "Aptitud:", aptitud(mejor_individuo))
-        print("_________________________________________________________________________________")
+
+        resultados.append([generacion + 1, aptitud_total, int(mejor_individuo, 2), aptitud(mejor_individuo)])
 
     return max(poblacion, key=aptitud)
 
 ###################################################################
-# algoritmo genetico ejecucion principal
+# Algoritmo genético ejecución principal
 ###################################################################
-print("_________________________________________________________________________________")
-print("_________________________________________________________________________________")
-print()
+print("\n")
+resultados = []
 mejor_solucion = algoritmo_genetico(TAMANIO_POBLACION, LONGITUD_CROMOSOMA, TASA_MUTACION, TASA_CRUCE, GENERACIONES)
-print("Mejor solución:", int(mejor_solucion, 2), "Aptitud:", aptitud(mejor_solucion))
+
+headers = ["Generación #", "Aptitud Total", "Mejor Individuo", "Aptitud del Mejor Individuo"]
+print(tabulate(resultados, headers=headers, tablefmt="grid"))
+
+print("\n* Mejor solución:", int(mejor_solucion, 2), "\n* Aptitud:", aptitud(mejor_solucion))
+
